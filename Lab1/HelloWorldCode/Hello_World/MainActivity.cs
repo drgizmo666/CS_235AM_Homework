d767c10a87f4b5e2aa02811e53fcda38941f936c
drgizmo666 <james_e_powell@live.com>
2016-03-28 11:34:33 -0700

    lab1
    Add part 1 of lab 1

---
+++ Lab1/HelloWorldCode/Hello_World/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Hello_World
+{
+	[Activity (Label = "Hello_World", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			//create the user interface in code
+			var layout = new LinearLayout (this);
+			layout.Orientation = Orientation.Vertical;
+
+			var aLabel = new TextView (this);
+
+			aLabel.SetText(Resource.String.helloLabelText);
+
+			var aButton = new Button (this);
+			aButton.SetText(Resource.String.helloButtonText);
+			aButton.Click += (sender, e) => {
+				aLabel.Text = "Hello from the button";
+			};
+			layout.AddView (aLabel);
+			layout.AddView (aButton);
+			SetContentView (layout);
+		}
+	}
+}
+
+
