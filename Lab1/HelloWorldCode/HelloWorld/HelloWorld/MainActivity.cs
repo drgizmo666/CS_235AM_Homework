34c2dd7b29bd48e1c80a9b950553b402c7821254
drgizmo666 <james_e_powell@live.com>
2016-03-30 12:52:30 -0700

    lab1
    fixed lab 1 it works woot

---
+++ Lab1/HelloWorldCode/HelloWorld/HelloWorld/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloWorld
+{
+	[Activity (Label = "HelloWorld", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			//create the user interface in code
+			var layout = new LinearLayout(this);
+			layout.Orientation = Orientation.Vertical;
+
+			var aLabel = new TextView (this);
+			//Old Code
+			//aLabel.Text = "Hello, Xamarin.Android";
+			//Set aLabel text using the xml string resources
+			aLabel.SetText(Resource.String.helloLabelText);
+
+			var aButton = new Button (this);
+			var bButton = new Button (this);
+			bButton.Enabled = false;
+			//old code
+			//aButton.Text = "Say Hello";
+			//Set aButton Text using the xml string resources
+			aButton.SetText(Resource.String.helloButtonText);
+			bButton.Text = "Reset";
+
+			aButton.Click += (sender, e) => {
+				aLabel.Text = "Hello from button";
+				aButton.Enabled = false;
+				bButton.Enabled = true;
+			};
+
+			bButton.Click += (sender, e) => {
+				aLabel.SetText(Resource.String.helloLabelText);
+				aButton.Enabled = true;
+				bButton.Enabled = false;
+			};
+
+			layout.AddView (aLabel);
+			layout.AddView (aButton);
+			layout.AddView (bButton);
+			SetContentView (layout);
+		}
+	}
+}
+
+
---
+++ Lab1/HelloWorldXML/SayHello/SayHello/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SayHello
+{
+	[Activity (Label = "SayHello", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			var layout = new LinearLayout (this);
+			layout.Orientation = Orientation.Vertical;
+
+			var aLabel = new TextView (this);
+		}
+	}
+}
+
+
