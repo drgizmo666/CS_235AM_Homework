d5c323561a3a74ad6fab56113607134b9768391f
drgizmo666 <james_e_powell@live.com>
2016-04-04 12:52:01 -0700

    lab 2
    Started lab 2

---
+++ Lab2/BottlesOfBeer/BottlesOfBeer/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace BottlesOfBeer
+{
+	[Activity (Label = "Bottles Of Beer", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			//USE UI CREATED IN MAIN.AXML
+			SetContentView(Resource.Layout.Main);
+			var takeOne = FindViewById<Button> (Resource.Id.takeOne);
+			var takeTwo = FindViewById<Button> (Resource.Id.takeTwo);
+
+			takeOne.Click += delegate {
+				//StartActivity (typeof(WallActivity));
+				var tooTheWall = new Intent(this, typeof(WallActivity));
+				tooTheWall.PutExtra("FirstData", 1);
+				StartActivity (tooTheWall);
+			};
+
+			takeTwo.Click += (sender, e) => {
+				StartActivity (typeof(WallActivity));
+			};
+
+		}
+	}
+}
+
+
---
+++ Lab2/BottlesOfBeer/BottlesOfBeer/WallActivity.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BottlesOfBeer
+{
+	[Activity (Label = "Bottles Of Beer")]			
+	public class WallActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// SHOW THE 2ND LAYOUT
+			//USE UI CREATED IN MAIN.AXML
+			SetContentView(Resource.Layout.Wall);
+			var bottlesTaken = Intent.GetIntExtra("FirstData", 0);
+			var showSecond = FindViewById<Button> (Resource.Id.showMain);
+			var wallLabel = FindViewById<TextView> (Resource.Id.Screen2Label);
+
+			wallLabel.Text = bottlesTaken.ToString ();
+			showSecond.Click += (sender, e) => {
+				StartActivity (typeof(MainActivity));
+			};
+		}
+	}
+}
+
