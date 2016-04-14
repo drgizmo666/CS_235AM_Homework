1692716df19410ceed63c389bdaefd7fc78c72d1
drgizmo666 <james_e_powell@live.com>
2016-04-13 17:08:24 -0700

    lab3
    added riddles for lab 3

---
+++ Lab 3/RiddleMeThis/RiddleMeThis/Riddles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RiddleMeThis
+{
+	public class Riddles
+	{
+		public Riddles ()
+		{
+			var riddleOne = "When you have me, you feel like sharing me but if you do share me, you don't have me what am I?";
+			var answerOne = "Secret";
+			var riddleTwo = "I am the beginning of eternity, the end of time and space, the beginning of the end and the end of every space. What am I?";
+			var answerTwo = "e";
+			var riddleThree = "I have a bed, but I do not sleep, I have a mouth but I don't eat. You hear me whisper, but I never talk. You can see me run, yet I never walk. What am I?";
+			var answerThree = "river";
+		}
+	}
+}
+
