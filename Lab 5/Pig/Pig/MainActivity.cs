3fac5c8d6c0fcec6ac6f0b4e2613a55385f6b8b9
drgizmo666 <james_e_powell@live.com>
2016-05-07 22:45:06 -0700

    lab 5
    almost finished does not save state

--- Lab 5/Pig/Pig/MainActivity.cs
+++ Lab 5/Pig/Pig/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Widget;
 using Android.OS;
 using System;
+using Android.Views;
 
 namespace Pig
 {
@@ -10,6 +11,17 @@ namespace Pig
 	{
 		public PigLogic oink = new PigLogic ();
 		public string whosTurn = "p1";
+		public int numRolled;
+
+		Button rollButton;
+		Button scoreButton;
+		Button newGameButton;
+		EditText player1Name;
+		EditText player2Name;
+		TextView tempScoreText;
+		TextView player1Score;
+		TextView player2Score;
+		TextView outputTextView;
 
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
@@ -21,40 +33,92 @@ namespace Pig
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button rollButton = FindViewById<Button> (Resource.Id.rollDiceButton);
-			Button scoreButton = FindViewById<Button> (Resource.Id.endTurnButton);
-			var tempScoreText = FindViewById<TextView> (Resource.Id.tempScoreTextView);
-			var player1Score = FindViewById<TextView> (Resource.Id.player1ScoreTextView);
-			var player2Score = FindViewById<TextView> (Resource.Id.player2ScoreTextView);
-			var outputTextView = FindViewById<TextView> (Resource.Id.outputTextView);
+			rollButton = FindViewById<Button> (Resource.Id.rollDiceButton);
+			scoreButton = FindViewById<Button> (Resource.Id.endTurnButton);
+			newGameButton = FindViewById<Button> (Resource.Id.newGameButton);
+			player1Name = FindViewById<EditText> (Resource.Id.player1EditText);
+			player2Name = FindViewById<EditText> (Resource.Id.player2EditText);
+			tempScoreText = FindViewById<TextView> (Resource.Id.tempScoreTextView);
+			player1Score = FindViewById<TextView> (Resource.Id.player1ScoreTextView);
+			player2Score = FindViewById<TextView> (Resource.Id.player2ScoreTextView);
+			outputTextView = FindViewById<TextView> (Resource.Id.outputTextView);
+
+			rollButton.Visibility = ViewStates.Invisible;
+			scoreButton.Visibility = ViewStates.Invisible;
+
+			newGameButton.Click += delegate {
+				if(player1Name.Text=="" || player2Name.Text=="")
+				{
+					oink.createPlayers("Beavis", "Butt-Head");
+					player1Name.Text = oink.getPlayer1Name();
+					player2Name.Text = oink.getPlayer2Name();
+					outputTextView.Text = oink.getPlayer1Name() + "'s turn";
+					player1Name.Enabled = false;
+					player2Name.Enabled = false;
+					rollButton.Visibility = ViewStates.Visible;
+					scoreButton.Visibility = ViewStates.Visible;
+					newGameButton.Visibility = ViewStates.Invisible;
+				}
+				else
+				{
+					oink.createPlayers(player1Name.Text, player2Name.Text);
+					player1Name.Text = oink.getPlayer1Name();
+					player2Name.Text = oink.getPlayer2Name();
+					outputTextView.Text = oink.getPlayer1Name() + "'s turn";
+					player1Name.Enabled = false;
+					player2Name.Enabled = false;
+					rollButton.Visibility = ViewStates.Visible;
+					scoreButton.Visibility = ViewStates.Visible;
+					newGameButton.Visibility = ViewStates.Invisible;
+				}
+				
+			};
 
 			rollButton.Click += delegate {
-				setImageView();
+				numRolled = oink.getNumber();
+
+				if(numRolled == 1)
+				{
+					Android.Widget.Toast.MakeText(this,
+						"Sorry, you rolled a 1 and lost all your points earned during this turn.",
+						Android.Widget.ToastLength.Short).Show();
+					
+					if(whosTurn == "p1")
+					{
+						whosTurn = "p2";
+						outputTextView.Text = oink.getPlayer2Name() + "'s turn";
+						setImageView();
+					}
+					else
+					{
+						whosTurn = "p1";
+						outputTextView.Text = oink.getPlayer1Name() + "'s turn";
+						setImageView();
+					}
+				}
+				else{ setImageView(); }
 			};
 
 			scoreButton.Click += delegate {
-				if(whosTurn == "p1")
-				{
-					player1Score.Text = oink.saveScore(Convert.ToInt32(tempScoreText.Text), whosTurn).ToString();
-					tempScoreText.Text = oink.showTempScore();
-					outputTextView.Text = "Player 2 Turn";
-					whosTurn = "p2";
-				}
+				oink.saveScore(whosTurn);
+				reDrawScreen();
+				if(oink.checkWin())
+					endGame();
 				else{
-					player2Score.Text = oink.saveScore(Convert.ToInt32(tempScoreText.Text), whosTurn).ToString();
-					tempScoreText.Text = oink.showTempScore();
-					outputTextView.Text = "Player 1 Turn";
-					whosTurn = "p1";
+					whosTurn = oink.switchPlayer(whosTurn);
+					if(whosTurn == "p1")
+						outputTextView.Text = oink.getPlayer1Name() + "'s turn";
+					else
+						outputTextView.Text = oink.getPlayer2Name() + "'s turn";
 				}
+					
 			};
 		}
 
 		public void setImageView()
 		{
-			var tempScoreText = FindViewById<TextView> (Resource.Id.tempScoreTextView);
 			var diceImageView = FindViewById<ImageView> (Resource.Id.DiceImageView);
-			var numRolled = oink.getNumber ();
-
+		
 			switch (numRolled) {
 			case(1):
 					diceImageView.SetImageResource (Resource.Drawable.one);
@@ -91,6 +155,49 @@ namespace Pig
 					tempScoreText.Text = "You Rolled a " + numRolled.ToString () + " You Hacker";
 					break;
 			}
+		}
+
+		public void endGame()
+		{
+			string p1Win = oink.getPlayer1Name() + "'s has got a 100 points, " + oink.getPlayer2Name() + " You have one more turn to win";
+				
+			if (whosTurn == "p1") {
+				Android.Widget.Toast.MakeText (this,
+					p1Win,
+					Android.Widget.ToastLength.Short).Show ();
+				whosTurn = oink.switchPlayer (whosTurn);
+				reDrawScreen ();
+			} else {
+				outputTextView.Text = oink.winnerHamDinner ();
+				resetGame ();
+			}
+		}
+
+		public void reDrawScreen()
+		{
+			tempScoreText.Text = "0";
+			player1Score.Text = oink.getP1Score ().ToString ();
+			player2Score.Text = oink.getP2Score ().ToString ();
+
+			if (whosTurn == "p1") {
+				outputTextView.Text = oink.getPlayer1Name() + "'s turn";
+			}
+			else
+				outputTextView.Text = oink.getPlayer2Name() + "'s turn";
+		}
+
+		public void resetGame()
+		{
+			player1Name.Enabled = true;
+			player2Name.Enabled = true;
+			rollButton.Visibility = ViewStates.Invisible;
+			scoreButton.Visibility = ViewStates.Invisible;
+			newGameButton.Visibility = ViewStates.Visible;
+
+			player1Score.Text = "0";
+			player2Score.Text = "0";
+
+			whosTurn = "p1";
 		}
 	}
 }
--- Lab 5/Pig/Pig/PigLogic.cs
+++ Lab 5/Pig/Pig/PigLogic.cs
@@ -4,11 +4,18 @@ namespace Pig
 {
 	public class PigLogic
 	{
+		public class Player
+		{
+			public string name { get; set;}
+			public int score { get; set;}
+		}
+
 		Random d6 = new Random();
+
 		int tempScore;
 
-		int player1Score;
-		int player2Score;
+		Player  p1 = new Player();
+		Player p2 = new Player();
 
 		public PigLogic ()
 		{
@@ -38,17 +45,71 @@ namespace Pig
 			return tempScore.ToString();
 		}
 
-		public int saveScore(int score, string turn)
+		public void saveScore(string turn)
 		{
 			if (turn == "p1") {
-				player1Score += score;
+				p1.score += tempScore;
 				emptyTempScore ();
-				return player1Score;
 			} else {
-				player2Score += score;
+				p2.score += tempScore;
 				emptyTempScore ();
-				return player2Score;
 			}
+		}
+
+		public string switchPlayer(string Turn)
+		{
+			if (Turn == "p1") {
+				return "p2";
+			} else {
+				return "p1";
+			}
+		}
+
+		public void createPlayers(string player1, string player2)
+		{
+			//create player 1
+			p1.name = player1;
+			p1.score = 0;
+			//create player 2;
+			p2.name = player2;
+			p2.score = 0;
+		}
+
+		public string getPlayer1Name()
+		{
+			return p1.name;
+		}
+
+		public string getPlayer2Name()
+		{
+			return p2.name;
+		}
+
+		public int getP1Score()
+		{
+			return p1.score;
+		}
+
+		public int getP2Score()
+		{
+			return p2.score;
+		}
+
+		public bool checkWin()
+		{
+			if (p1.score >= 100 || p2.score >= 100) 
+				return true;
+			else
+				return false;
+
+		}
+
+		public string winnerHamDinner()
+		{
+			if (p1.score > p2.score)
+				return p1.name + " is the winner";
+			else
+				return p2.name + " is the winner";
 		}
 	}
 }
