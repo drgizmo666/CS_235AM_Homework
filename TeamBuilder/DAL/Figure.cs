949ac6b7b9e3985085216b816d119c5b821177f0
drgizmo666 <james_e_powell@live.com>
2016-05-18 10:49:42 -0700

    final setup
    setup my final and finished the first xml file

---
+++ TeamBuilder/DAL/Figure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SQLite;
+
+namespace DAL
+{
+	[Table("Figures")]
+	public class Figure
+	{
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+		public string setName { get; set;}
+		public string setNumber { get; set;}
+		public string name { get; set;}
+		public string rarity { get; set;}
+		public string keywords { get; set;}
+		public int points { get; set;}
+		public int lifeClicks { get; set;}
+		public string affiliation { get; set;}
+		public string specialPowers{ get; set;}
+	}
+}
+
